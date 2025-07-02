8f7883bd3fb2af416177e2224feab50e44c54d72
Chukwuka Uba <promiseuba67@gmail.com>
2025-07-02 14:23:31 +0100

    test: lifinity adapter single-leg path

---
+++ tests/adapter_lifinity.test.ts
@@ -0,0 +1,34 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Aggregator } from "../target/types/aggregator";
+import { setupTokenAccounts, provider } from "./utils";
+import { ComputeBudgetProgram } from "@solana/web3.js";
+
+const program = anchor.workspace.aggregator as Program<Aggregator>;
+
+describe("adapter – lifinity", () => {
+  it("single leg executes", async () => {
+    const { ata } = await setupTokenAccounts();
+    const cuIx = ComputeBudgetProgram.setComputeUnitLimit({ units: 1_400_000 });
+
+    const leg = {
+      dexId: { lifinityV2: {} } as any,
+      inAmount: new anchor.BN(100),
+      minOut: new anchor.BN(90),
+      accountCount: 0,
+      data: Buffer.alloc(0),
+    };
+
+    await program.methods
+      .route([leg], new anchor.BN(100), new anchor.BN(80), 0)
+      .accounts({
+        userAuthority: provider.wallet.publicKey,
+        userSource: ata,
+        userDestination: ata,
+        feeVault: ata,
+        computeBudget: ComputeBudgetProgram.programId,
+      })
+      .preInstructions([cuIx])
+      .rpc();
+  });
+});
