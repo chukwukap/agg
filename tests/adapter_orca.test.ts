81d024373858c681c5299cfc7443205d6fb18a8e
Chukwuka Uba <promiseuba67@gmail.com>
2025-07-02 14:25:06 +0100

    test: orca adapter single-leg

---
+++ tests/adapter_orca.test.ts
@@ -0,0 +1,34 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Aggregator } from "../target/types/aggregator";
+import { setupTokenAccounts, provider } from "./utils";
+import { ComputeBudgetProgram } from "@solana/web3.js";
+
+const program = anchor.workspace.aggregator as Program<Aggregator>;
+
+describe("adapter – orca", () => {
+  it("single leg executes", async () => {
+    const { ata } = await setupTokenAccounts();
+    const cuIx = ComputeBudgetProgram.setComputeUnitLimit({ units: 1_400_000 });
+
+    const leg = {
+      dexId: { orcaWhirlpool: {} } as any,
+      inAmount: new anchor.BN(200),
+      minOut: new anchor.BN(180),
+      accountCount: 0,
+      data: Buffer.alloc(0),
+    };
+
+    await program.methods
+      .route([leg], new anchor.BN(200), new anchor.BN(150), 0)
+      .accounts({
+        userAuthority: provider.wallet.publicKey,
+        userSource: ata,
+        userDestination: ata,
+        feeVault: ata,
+        computeBudget: ComputeBudgetProgram.programId,
+      })
+      .preInstructions([cuIx])
+      .rpc();
+  });
+});
