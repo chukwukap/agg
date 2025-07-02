1a291ffed016ee87b9916a02a92350c5e409f404
Chukwuka Uba <promiseuba67@gmail.com>
2025-07-02 14:27:02 +0100

    test: add solar_cp, solar_clmm, invariant adapter tests

---
+++ tests/adapter_solar_cp.test.ts
@@ -0,0 +1,34 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Aggregator } from "../target/types/aggregator";
+import { setupTokenAccounts, provider } from "./utils";
+import { ComputeBudgetProgram } from "@solana/web3.js";
+
+const program = anchor.workspace.aggregator as Program<Aggregator>;
+
+describe("adapter - solar_cp", () => {
+  it("single leg executes", async () => {
+    const { ata } = await setupTokenAccounts();
+    const cuIx = ComputeBudgetProgram.setComputeUnitLimit({ units: 1_400_000 });
+
+    const leg = {
+      dexId: { solarCp: {} } as any,
+      inAmount: new anchor.BN(300),
+      minOut: new anchor.BN(270),
+      accountCount: 0,
+      data: Buffer.alloc(0),
+    };
+
+    await program.methods
+      .route([leg], new anchor.BN(300), new anchor.BN(250), 0)
+      .accounts({
+        userAuthority: provider.wallet.publicKey,
+        userSource: ata,
+        userDestination: ata,
+        feeVault: ata,
+        computeBudget: ComputeBudgetProgram.programId,
+      })
+      .preInstructions([cuIx])
+      .rpc();
+  });
+});
